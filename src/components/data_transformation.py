cb7b250b71c820c95642b015e7f0a524142d8ec4
Ritesh Sil <sil.ritzzz@gmail.com>
2023-06-25 23:51:37 +0530

    data tranformation

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -25,11 +25,71 @@ class DataTransformation:
 
 
     def get_data_transformer_object(self):
+        ''' This function is responsible for data transformation'''
         try:
+            numerical_column = [
+                'reading_score',
+                'writing_score'
+
+            ]
+
+            categorical_column = [
+
+                'gender',
+                'race_ethnicity',
+                'parental_level_of_education',
+                'lunch',
+                'test_preparation_course'
+
+            ]
+
+            num_pipeline =Pipeline(
+                steps = [
+                    ("imputer",SimpleImputer(strategy="median")),
+                    ("scaler",StandardScaler()) 
+                ]
+            )
+
+
+            cat_pipeline=Pipeline(
+
+                steps=[
+                    ("imputer",SimpleImputer(strategy="most_frequent")),
+                    ("one_hot_encoder",OneHotEncoder()),
+                    ("scaler", StandardScaler(with_mean=False))
+                ]
+            )
+
+
+            logging.info(f"Categorical columns:{categorical_column}")
+            logging.info(f"Categorical columns: {categorical_column}")
+
+
+            preprocessor = ColumnTransformer(
+                [
+                    ("num_pipeline",num_pipeline,numerical_column),
+                    ("cat_pipeline",cat_pipeline,categorical_column)
+                ]
+            )
+
+
+            return preprocessor
+        
+
+
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+
+
+    def initiate_data_transformation(self, train_path, test_path):
+
+        try:
+            pass
+
+        except Exception as e:
+            pass
             
-            pass
-        except:
-            pass
 
 
 
